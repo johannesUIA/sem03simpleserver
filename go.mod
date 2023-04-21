c03b506db7bcf4a050e08084e1186997adc129a9
johannes <johannesma@student.uia.no>
2023-04-21 08:55:41 +0000

    ny kloning

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/johannesUIA/sem03simpleserver
+
+go 1.18
--- tcp_server.go
+++ tcp_server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"sync"
+"github.com/johannesUIA/is105sem03/mycrypt"
 )
 
 func main() {
@@ -36,7 +37,15 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
+					
+
+
+      dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                     log.Println("Dekrypter melding: ", string(dekryptertMelding))
+                                msg := string(dekryptertMelding)
+                                        switch msg {
+
+
   				        case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
